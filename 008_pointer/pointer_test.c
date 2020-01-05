3d7d7c1ac6f0a93c18dbbe2224a23fd18fbe19e2
kk-1988_vmware <xiaobo.kong@aliyun.com>
2020-01-06 06:52:51 +0800

    修改指针相关操作源码，添加字符数组和int数组相关操作

--- 008_pointer/pointer_test.c
+++ 008_pointer/pointer_test.c
@@ -57,6 +57,53 @@ void test1()
 	printf("*pc = 0x%x\r\n",*pc);
 }
 
+void test2()
+{
+	char ca[3] = {'A','B','C'};
+	char *pc;
+
+	/* 所有变量都保存在内存中,用&取地址符 */
+	printf("ca = %p\r\n",ca);
+	printf("&pc = %p\r\n",&pc);
+
+	printf("***********************************\r\n");
+
+	/* 变量保存某些值 */
+	pc = ca;
+	printf("pc = %p\r\n",pc);
+	printf("***********************************\r\n");
+
+	/* 使用指针 */
+	printf("*pc = %c\r\n",*pc);
+	pc++;
+	printf("*pc = %c\r\n",*pc);
+	pc++;
+	printf("*pc = %c\r\n",*pc);	
+}
+
+void test3()
+{
+	int ia[3] = {0x12345678,0x87654321,0x13572468};
+	//易错点,int类型表示++移动int字节的数字，所以一次移动4个字节
+	int *pi;
+
+	/* 所有变量都保存在内存中,用&取地址符 */
+	printf("ia = %p\r\n",ia);
+	printf("&pi = %p\r\n",&pi);
+
+	/* 变量保存某些值 */
+	pi = ia;
+	printf("pi = %p\r\n",pi);
+	printf("***********************************\r\n");
+
+	/* 使用指针 */
+	printf("*pi = 0x%x\r\n",*pi);
+	pi++;
+	printf("*pi = 0x%x\r\n",*pi);
+	pi++;
+	printf("*pi = 0x%x\r\n",*pi);	
+}
+
 int main(int argc,char *argv[])
 {
 	printf("sizeof(char) = %d\r\n",sizeof(char));
@@ -65,7 +112,9 @@ int main(int argc,char *argv[])
 	printf("sizeof(char **) = %d\r\n",sizeof(char **));
 	printf("============================================\r\n");
 	//test0();
-	test1();
+	//test1();
+	//test2();
+	test3();
 
 	return 0;
 }
