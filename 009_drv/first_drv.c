213692c7faadacbdc453d820c6d3c74a82c8ddf3
kk-1988_vmware <xiaobo.kong@aliyun.com>
2020-01-13 07:12:18 +0800

    修改第一个驱动程序

--- 009_drv/first_drv.c
+++ 009_drv/first_drv.c
@@ -12,15 +12,23 @@
 static struct class *firstdrv_class;
 static struct class_device *firstdrv_class_dev;
 
+volatile unsigned long *gpbcon = 0;
+volatile unsigned long *gpbdat = 0;
+
 static int first_drv_open(struct inode *inode,struct file *file)
 {
 	printk("first drv open\n");
+	/* 配置GPB为输出 */
+	gpbcon &= ~();
 	return 0;	
 }
 
 static ssize_t first_drv_write(struct file *file, const char _user *buf,size_t count,loff_t *ppos)
 {
 	printk("first drv write\n");
+	int val;
+	/* 对相关引脚拉高拉低 */
+	copy_from_user(&val, buf, count);
 	return 0;
 }
 
@@ -33,12 +41,16 @@ static struct file_operations first_drv_fops = {
 int first_drv_init(void)
 {
 	register_chrdev(111, "first_drv", first_drv_fops);		//注册驱动程序(告诉内核)
+	gpbcon = (volatile unsigned long *)ioremap(0x56000050,16);
+	gpbdat = gpbcon + 1;
+	
 	return 0;
 }
 
 void first_drv_exit(void)
 {
 	unregister_chrdev(111, "first_drv");		//卸载驱动程序
+	iounremap(gpbcon);
 	return 0;
 }
 
