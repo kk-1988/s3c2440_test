4f3931943e2513f683673826b158b7ebdd5e2524
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-03-15 10:36:23 +0800

    4字节对齐改进

--- 015_relocate/007/main.c
+++ 015_relocate/007/main.c
@@ -16,11 +16,26 @@ int main(void)
 	printHex(g_A);
 	puts("\n\r");
 
+	//在链接脚本中设置bss段位4字节对齐，否则由于start.S中已经
+	//修改了ldrb为ldr，即从1字节变为4字节，这样导致bss段需要4字节对齐
+	//所以擦除掉了data的数据
 	while(1)
 	{
+	#if 0
+		puts("\n\rg_char = ");
+		printHex(g_char);
+		puts("\n\r");
+	#endif
+		
 		putchar(g_char);
 		g_char++;
 
+	#if 0
+		puts("\n\rg_char3 = ");
+		printHex(g_char3);
+		puts("\n\r");
+	#endif
+	
 		putchar(g_char3);
 		g_char3++;
 		
