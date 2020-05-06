09ecc46cabd34d4eae98d7fe82dadab09477faaa
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-07 07:17:24 +0800

    完善驱动

--- 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc.c
+++ 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc.c
@@ -2,12 +2,14 @@
 
 void adc_init()
 {
-	ADCCON = (1 << 14) | (49 << 6) |; | 
+	ADCCON = (1 << 14) | (49 << 6) | (0 << 3); 
+	ADCDLY = 0xff;
 }
 
 int adc_read_ain0()
 {
-	
+	ADCCON |= (1 << 0);
+	while(!(ADCCON & (1 << 15));	/* 等待ADC结束 */
+
+	return ADCDAT0 & 0x3ff;
 }
-
-
--- 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc_test.c
+++ 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc_test.c
@@ -1,12 +1,16 @@
-
+#include "adc.h"
 
 void adc_test(void)
 {
+	int val;
+	double vol;
+	
 	adc_init();
 
 	while(1)
 	{
-		adc_read_ain0();
+		val = adc_read_ain0();
+		vol = 		/* 1023 == 3.3v */
 
 		/* 在串口上打印出来 */
 
