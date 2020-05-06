826690f11f0b57ae99d427cd34fcd3757417e1cd
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-05-07 07:33:50 +0800

    完善驱动

--- 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc_test.c
+++ 022_adc_touchscreen_018/001_adc_018_002/adc_touchscreen/adc_test.c
@@ -4,6 +4,8 @@ void adc_test(void)
 {
 	int val;
 	double vol;
+	int m;		/* 整数部分 */
+	int n;		/* 小数部分 */
 	
 	adc_init();
 
@@ -11,8 +13,12 @@ void adc_test(void)
 	{
 		val = adc_read_ain0();
 		vol = val/1023*3.3;		/* 1023 == 3.3v */
-
+		m = (int)vol;		/* 3 */
+		vol = vol - m;		/* 小数部分 */
+		n = vol * 100;		/* 300 */
+		
 		/* 在串口上打印出来 */
+		printf("%d.%dv",m,n);
 
 		/* 在LCD上打印 */
 		
